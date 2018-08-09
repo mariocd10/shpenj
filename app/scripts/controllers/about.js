5c16c6615168c154521330b5219a1f40f6ffe2eb
Mario DeLaPaz <mariocd10@gmail.com>
2018-08-09 18:48:52 -0400

    Initial commit

---
+++ app/scripts/controllers/about.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name shpenjApp.controller:AboutCtrl
+ * @description
+ * # AboutCtrl
+ * Controller of the shpenjApp
+ */
+angular.module('shpenjApp')
+  .controller('AboutCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
---
+++ app/scripts/controllers/main.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name shpenjApp.controller:MainCtrl
+ * @description
+ * # MainCtrl
+ * Controller of the shpenjApp
+ */
+angular.module('shpenjApp')
+  .controller('MainCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
