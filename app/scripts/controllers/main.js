d8d9f1a0a78c88c04ea73969988890f33d992fa9
Mario DeLaPaz <mariocd10@gmail.com>
2018-08-13 00:40:47 -0400

    Add Upcoming Events, Title, Pillars

--- app/scripts/controllers/main.js
+++ app/scripts/controllers/main.js
@@ -8,10 +8,19 @@
  * Controller of the shpenjApp
  */
 angular.module('shpenjApp')
-  .controller('MainCtrl', function () {
+  .controller('MainCtrl', function ($scope, $location, $anchorScroll) {
     this.awesomeThings = [
       'HTML5 Boilerplate',
       'AngularJS',
       'Karma'
     ];
+
+    $scope.gotoTop = function(){
+    	//set location.hash to the id of
+    	// the element you wish to scroll to.
+    	$location.hash('Top');
+
+    	//call $anchorScroll()
+    	$anchorScroll();
+    };
   });
