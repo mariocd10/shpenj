3bc2be9767201f77fea144ff25d211b81faf5799
Mario DeLaPaz <mariocd10@gmail.com>
2018-08-28 22:46:03 -0400

    feature: Change Background in Navbar Item upon hover effect - Will allow users to see what navbar item is being selected

---
+++ app/scripts/script.js
@@ -0,0 +1,5 @@
+"use strict";
+$('#topheader .navbar-nav a').on('click', function () {
+	$('#topheader .navbar-nav').find('li.active').removeClass('active');
+	$(this).parent('li').addClass('active');
+});
