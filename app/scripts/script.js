9906d646dc438d76aaad2bdaba9a6d949640ac85
Mario DeLaPaz <mariocd10@gmail.com>
2018-09-08 23:47:44 -0400

    remove: deleted script.js

--- app/scripts/script.js
+++
@@ -1,5 +0,0 @@
-"use strict";
-$('#topheader .navbar-nav a').on('click', function () {
-	$('#topheader .navbar-nav').find('li.active').removeClass('active');
-	$(this).parent('li').addClass('active');
-});
