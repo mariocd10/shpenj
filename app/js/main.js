84e5f280f98068e8c6b3ff560b7de06e8fb37605
Mario DeLaPaz <mariocd10@gmail.com>
2018-09-08 23:20:09 -0400

    add: Add navbar and footer via js

--- app/js/main.js
+++ app/js/main.js
@@ -10,4 +10,31 @@
     portfolio website.
 */
 
-var bootstrap = require('bootstrap');
+function includeHTML() {
+  var section, i, elmnt, file, xhttp;
+  /*loop through a collection of all HTML elements:*/
+  section = document.getElementsByTagName("section");
+  for (i = 0; i < section.length; i++) {
+    elmnt = section[i];
+    /*search for elements with a certain atrribute:*/
+    file = elmnt.getAttribute("include-html");
+    if (file) {
+      /*make an HTTP request using the attribute value as the file name:*/
+      xhttp = new XMLHttpRequest();
+      xhttp.onreadystatechange = function() {
+        if (this.readyState == 4) {
+          if (this.status == 200) {elmnt.innerHTML = this.responseText;}
+          if (this.status == 404) {elmnt.innerHTML = "Page not found.";}
+          /*remove the attribute, and call this function once more:*/
+          elmnt.removeAttribute("include-html");
+          includeHTML();
+        }
+      } 
+      xhttp.open("GET", file, true);
+      xhttp.send();
+      /*exit the function:*/
+      return;
+    }
+  }
+}
+includeHTML();
