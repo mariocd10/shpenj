b63bf6878b6d5d16597cf819492e0f9895f6d44f
Mario DeLaPaz <mariocd10@gmail.com>
2020-05-20 22:30:15 -0400

    create renderList()

--- app/js/other/calendar.js
+++ app/js/other/calendar.js
@@ -1,14 +1,6 @@
 // test
 async function listEvents(){
     let event_pg = document.getElementById('event_pg');
-    let title = '';
-    let start_date = '';
-    let end_date;
-    let start_time = '';
-    let end_time = '';
-    let event_loc = '';
-    let event_reg_link = '';
-    let description = '';
     let api_key = "AIzaSyB-LTW9jsg9mCbvdClw0kAdgrTlWT1tcS0";
     let other = "83tln3112kb3t77cu43v4bg2og@group.calendar.google.com";
     let get_list = "https://www.googleapis.com/calendar/v3/calendars/"+other+"/events?key="+api_key;
@@ -21,34 +13,32 @@ async function listEvents(){
             return;
         }
 
-        for(e of value.items) {
-            description = e.description;
-            title = e.summary;
-            // location = e.location;
-            start_date = e.start.dateTime;
-            end_date = e.end.dateTime;
-            console.log('title: '+end_date);
-        }
-        console.log(value.items);
+        renderList(value.items);
     })
+};
+
+function renderList(events) {
+  let title, start_date, end_date, description;
+  for(e of events) {
+    description = e.description || '';
+    title = e.summary || '';
+    // location = e.location || '';
+    start_date = e.start.dateTime;
+    end_date = e.end.dateTime;
+  }
+  // console.log(events);
+};
+
+listEvents();
+
+function createEventCard(title, date, start, end, description){
+ let output = '<div class="card mb-3 card-event">'+ '<div class="row no-gutters">'+ '<div class="col-md-4">'+
+              '<img class="img-fluid" src="images/pillars/professional_dev.png" alt="Prof Event">'+'</div>'+
+              '<div class="col-md-8">'+'<div class="card-body">'+'<h4 class="card-title">'+title+'</h4>'+
+              '<div class="col-md-12"><i class="far fa-calendar-alt"></i>'+ date +'</div>'+
+              '<div class="col-md-12"><i class="fas fa-clock"></i> '+start+' - '+ end+'</div>'+
+              '<div class="col-md-12"><i class="fas fa-map-marker-alt"></i> '+location+'</div>'+
+              '<p class="card-text">'+description+'</p>'
+              '<a href="#" class="btn btn-primary">Go Somewhere</a></div></div></div></div>';
+return output;
 }
-//listEvents();
-/*
-<div class="card mb-3 card-event">
-      <div class="row no-gutters">
-        <div class="col-md-4">
-          <img class="img-fluid" src="images/pillars/professional_dev.png" alt="Prof Event">
-        </div>
-        <div class="col-md-8">
-          <div class="card-body">
-            <h4 class="card-title">Cafe Con Leche Series</h4>
-            <div class="col-md-12"><i class="far fa-calendar-alt"></i> 5/1/2020</div>
-            <div class="col-md-12"><i class="fas fa-clock"></i> 7:00PM - 8:00PM</div>
-            <div class="col-md-12"><i class="fas fa-map-marker-alt"></i> Zoom Call</div>
-            <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
-            <a href="#" class="btn btn-primary">Go somewhere</a>
-          </div>
-        </div>
-      </div>
-    </div>
-    */
